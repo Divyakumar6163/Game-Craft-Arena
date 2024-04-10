22a9ac627440aaf114ccf774b76422f0b453de59
Divya Kumar <divyakumar768800@gmail.com>
2024-04-10 20:18:44 +0530

    Modal Modified

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -3,11 +3,16 @@ import { useNavigate } from "react-router-dom";
 export default function Modal({ message, leftAttempts, onClose, finalScore }) {
   const navigate = useNavigate();
   function handleRestart() {
-    navigate("/levels");
+    navigate("/player1");
   }
   return (
     <dialog open className={style.modal}>
-      <h1 className={style.h1}>{message}</h1>
+      <h1
+        className={style.h1}
+        style={{ color: message === "You Won" ? "green" : "red" }}
+      >
+        {message}
+      </h1>
       <p className={style.p}>Attempts Left:{leftAttempts}</p>
       <p className={style.p}>Score:{finalScore}</p>
       <button onClick={onClose} className={style.button}>
