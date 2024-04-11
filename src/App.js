f40b0a7d8f0c0add944bd69743732557daad3eea
Divya Kumar <divyakumar768800@gmail.com>
2024-04-12 03:58:35 +0530

    toughness of level increases

--- src/App.js
+++ src/App.js
@@ -11,6 +11,16 @@ function App() {
     category: '',
     description: ''
   });
+  const [PlayerData2, setPlayerData2] = useState({
+    object: '',
+    category: '',
+    description: ''
+  });
+  const [PlayerData3, setPlayerData3] = useState({
+    object: '',
+    category: '',
+    description: ''
+  });
   const [attempts,setAttempts]=useState();
   const [playerName,setPlayerName]=useState({
     player1:'',
@@ -19,6 +29,13 @@ function App() {
   function PlayerDetail(player1){
     setPlayerData(player1);
   }
+  function PlayerDetail1(player2){
+    setPlayerData2(player2);
+  }
+  function PlayerDetail2(player3){
+    setPlayerData3(player3);
+  }
+
   function PlayerAttempts(attempts){
     setAttempts(attempts);
   }
@@ -35,8 +52,8 @@ function App() {
       <Route path="/" element={<GameRules/>}/>
       <Route path="/levels" element={<Levels playerNameData={(PlayerNameValue) => PlayerName(PlayerNameValue)} playerAttempts={(numAttempt) =>PlayerAttempts(numAttempt)}/>}/>
       {/* <Route path="/levels" element={<PlayerName/>}/> */}
-      <Route path="/player1" element={<Player1 player1Name={playerName.player1} playerDetail={(playerDetail)=>PlayerDetail(playerDetail)}/>}/>
-      <Route path="/player2" element={<Player2 player2Name={playerName.player2} dataReceived={PlayerData} attempts={attempts}/>}/>
+      <Route path="/player1" element={<Player1 player1Name={playerName.player1} playerDetail={(playerDetail)=>PlayerDetail(playerDetail)} playerDetail1={(playerDetail1)=>PlayerDetail1(playerDetail1)} playerDetail2={(playerDetail2)=>PlayerDetail2(playerDetail2)} attempts={attempts}/>}/>
+      <Route path="/player2" element={<Player2 player2Name={playerName.player2} dataReceived={PlayerData} dataReceived2={PlayerData2} dataReceived3={PlayerData3} attempts={attempts}/>}/>
       </Routes> 
      </BrowserRouter>
     </main>
