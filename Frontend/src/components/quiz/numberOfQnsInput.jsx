1fbdbd7d38912315f182a49fccd9d178b7f2f005
Divyakumar6163 <divyakumar768800@gmail.com>
2024-06-09 17:50:23 +0530

    css and fnctionality of create Quiz improved

---
+++ Frontend/src/components/quiz/numberOfQnsInput.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import styles from "./createQuiz.module.css";
+function NumberOfQnsInput({
+  setNumberOfQuestions,
+  setIsSubmitted,
+  numberOfQuestions,
+}) {
+  const [isClicked, setIsClicked] = useState(false);
+  return (
+    <form onSubmit={(e) => e.preventDefault()} className={styles.form}>
+      <div className={styles["form-group"]}>
+        <label className={styles.label}>Number of Questions</label>
+        <div className={styles.formArea}>
+          <input
+            required
+            type="number"
+            value={numberOfQuestions}
+            onChange={(e) => setNumberOfQuestions(Number(e.target.value))}
+          />
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              setIsClicked(true);
+              if (numberOfQuestions > 0) {
+                setIsSubmitted(true);
+              }
+            }}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+      {numberOfQuestions <= 0 && isClicked && (
+        <p className={styles.p}>*Please enter correct size of Quiz</p>
+      )}
+    </form>
+  );
+}
+
+export default NumberOfQnsInput;
