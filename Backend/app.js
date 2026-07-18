be0856dc6dd42aa345e12b45f4f854e6da11eb51
Divyakumar6163 <divyakumar768800@gmail.com>
2026-07-18 17:58:07 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,38 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const guessTheObjectRoutes = require("./routes/guessTheObjectRoute");
+const quizRoutes = require("./routes/quizRoute");
+
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:8000",
+  "https://game-craft-arena.vercel.app",
+  "https://game-craft-arena.onrender.com",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  }),
+);
+
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(morgan("dev"));
 
-const guessTheObjectRoutes = require("./routes/guessTheObjectRoute");
-const quizRoutes = require("./routes/quizRoute");
-
 app.use("/guess-the-object", guessTheObjectRoutes);
 app.use("/quiz", quizRoutes);
+
 app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
